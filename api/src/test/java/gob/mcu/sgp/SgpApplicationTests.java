d37291279abb84b098e7125122b84db2847a6d03
catalinito <tomascatalini@gmail.com>
2022-02-23 16:04:56 -0300

    Primer commit

---
+++ api/src/test/java/gob/mcu/sgp/SgpApplicationTests.java
@@ -0,0 +1,13 @@
+package gob.mcu.sgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
